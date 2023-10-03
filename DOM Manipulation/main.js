eb4e3f8f4cdbc88f1747210f8eeb4bfdae9f62ca
rhevagiant <moonjeonya@gmail.com>
2023-10-03 22:34:08 +0700

    modul 3

--- DOM Manipulation/main.js
+++ DOM Manipulation/main.js
@@ -24,3 +24,61 @@
 
 
 
+
+
+//MODUL 3
+
+const pBAru = document.createElement('p');
+const teksPBAru = document.createTextNode('Paragraf Baru');
+pBAru.appendChild(teksPBAru); //'Paragraf Baru' masuk kedalam element p
+
+const sectionA = document.getElementById('a');
+sectionA.appendChild(pBAru); //menyimpan pBaru di akhir section A
+
+
+// //insert before
+const liBaru = document.createElement('li');
+const teksLiBaru = document.createTextNode('Item Baru');
+liBaru.appendChild(teksLiBaru);
+
+const ul = document.querySelector('section#b ul');
+const li2 = document.querySelector('section#b ul li:nth-child(2)');
+
+// // atau menggunakan
+// // const li2 = ul.querySelector('li:nth-child(2)');
+// // untuk lebih ringkas
+
+ul.insertBefore(liBaru, li2);
+
+
+//remove
+// const section_A = document.getElementById('a');
+// const link = document.getElementsByTagName('a') [0];
+// section_A.removeChild(link);
+
+
+// //replace
+const section_B = document.getElementById('b');
+const p4 = section_B.querySelector('p');
+
+const h2Baru = document.createElement('h2');
+const teksH2Baru = document.createTextNode('Judul Baru!!!');
+
+h2Baru.appendChild(teksH2Baru);
+
+section_B.replaceChild(h2Baru, p4); //yang baru dulu lalu yang mau diganti
+
+pBAru.style.backgroundColor = 'lightblue';
+liBaru.style.backgroundColor = 'lightblue';
+h2Baru.style.backgroundColor = 'lightblue';
+
+
+//event handler
+// const p3 = document.getElementsByClassName('p3');
+// p3.onclick = function(){
+//     p3.style.backgroundColor = 'lightgreen';
+// }
+
+
+
+
